2cf2ed52ee488b84e3704572918e07ffb0fb3520
geki <h.mth@web.de>
2021-05-24 16:47:15 +0200

    scripts/transcode_10bit_to_8bit_hardsubs.sh: Update.

--- scripts/transcode_10bit_to_8bit_hardsubs.sh
+++ scripts/transcode_10bit_to_8bit_hardsubs.sh
@@ -11,13 +11,19 @@ die()
 
 transcode_10to8_hardsubs()
 {
-	local p d f o s t
+	local b p d t f o c s
 
+	local src_fmt="mkv"
+	local dst_fmt="mp4"
+	local dst_crc
+
+	b="$(basename ${0})"
 	p="$(realpath "${1}")"
 	d="${p//10bit/8bit}"
+	t="$(printf '%q' "${d}")"
 
-	ls -1 "${p}"/*.mkv \
-		|| die "no mkv files found in directory: ${p}"
+	ls -1 "${p}"/*.${src_fmt} \
+		|| die "no ${src_fmt} files found in directory: ${p}"
 
 	mkdir -p "${d}" \
 		|| die "failed to create destination path: ${d}"
@@ -25,35 +31,72 @@ transcode_10to8_hardsubs()
 	mkdir -p "${d}/fonts" \
 		|| die "failed to create destination fonts path: ${d}"
 
-	local src_fmt="mkv"
-	local dst_fmt="mkv"
+	# ship script with batch to recreate
+	cp -v "${0}" "${d}/${b}"
+
+	# log transcode
+	ffmpeg -version >"${d}/${b}.log"
+	echo "" >"${d}/${b}.log"
+	ls -1 "${p}"/*.${src_fmt} \
+		>"${d}/${b}.log"
+	echo "" >"${d}/${b}.log"
 
 	for f in "${p}"/*.${src_fmt}
 	do
 		o="${f//10bit/8bit}"
 		o="${o//${src_fmt}/${dst_fmt}}"
 		s="$(printf '%q' "${f}")"
-		t="$(printf '%q' "${d}")"
 
 		# shell window title
-		if [ -x "$(which basename)" ]
-		then
-			echo -ne "\033]0;Transcode to "$(basename "${o}")"\007"
-		fi
+		echo -ne "\033]0;Transcode to "$(basename "${o}")"\007"
 
-		pushd "${d}/fonts" \
+		pushd "${d}/fonts" >/dev/null \
 			|| die "failed to push into destination fonts path"
-		ffmpeg -y -dump_attachment:t "" -i "${f}"
-		popd \
+		AV_LOG_FORCE_NOCOLOR=1 \
+		ffmpeg -loglevel quiet \
+			-y -dump_attachment:t "" \
+			-i "${f}" \
+			2>"${d}/${b}.log"
+		popd >/dev/null \
 			|| die "failed to pop out of destination fonts path"
 
-		ffmpeg -i "${f}" \
+		AV_LOG_FORCE_NOCOLOR=1 \
+		ffmpeg -loglevel level+warning \
+			-stats \
+			-i "${f}" \
 			-c:a copy \
-			-c:v libx264 -vf "subtitles=${s}:fontsdir=${t}/fonts,format=yuv420p" \
+			-c:v libx264 \
+			-vf "subtitles=${s}:fontsdir=${t}/fonts,format=yuv420p" \
 			-preset veryfast -tune animation -crf 23 -r 23.98 \
 			-map 0:a -map 0:v \
 			"${o}" \
+			2>"${d}/${b}.log" \
 			|| die "failed to transcode video from file: ${f}"
+
+		# crc32 8-digit
+		dst_crc="$(crc32 "${o}" | cut -f1 | tr '[a-z]' '[A-Z]')"
+		c="${o:0:-14}[${dst_crc}].${dst_fmt}"
+
+		echo "i: ${f}" \
+			| tee -a "${d}/${b}.log"
+		echo "o: ${c}" \
+			| tee -a "${d}/${b}.log"
+
+		mv "${o}" "${c}" \
+			|| die "failed to update crc32 verification of file: ${o}"
+	done
+}
+
+validate_helper_commands()
+{
+	local cmd
+
+	for cmd in basename crc32 cut ffmpeg ls mkdir realpath tee tr
+	do
+		if [ ! -x "$(which ${cmd})" ]
+		then
+			die "${cmd} binary not found"
+		fi
 	done
 }
 
@@ -62,21 +105,7 @@ then
 	die "parameter not a directory: ${1}"
 fi
 
-if [ ! -x "$(which ls)" ]
-then
-	die "ls binary not found"
-fi
-
-if [ ! -x "$(which realpath)" ]
-then
-	die "realpath binary not found"
-fi
-
-if [ ! -x "$(which ffmpeg)" ]
-then
-	die "ffmpeg binary not found"
-fi
-
+validate_helper_commands
 transcode_10to8_hardsubs "${1}"
 
 exit ${?}
